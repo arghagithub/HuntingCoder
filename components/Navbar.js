24a7e953f83716ae8f876e6fb40030f633ba190d
Argha <arghagolui@gmail.com>
2023-04-12 13:42:57 +0530

    video 14: displaying navbar in all page

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+const Navbar = () => {
+  return (
+    <nav className={styles.mainnav}>
+      <ul>
+        <Link href="/">
+          {" "}
+          <a><li>Home</li></a>
+        </Link>
+        <Link href="/about">
+          {" "}
+          <a><li>About</li></a>{" "}
+        </Link>
+        <Link href="/blog">
+          {" "}
+          <a><li>Blog</li></a>
+        </Link>
+        <Link href="/contact">
+          {" "}
+          <a><li>Contact</li></a>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
