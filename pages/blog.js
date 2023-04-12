6848302edb2c1ae26b275fac031b44b50332b3e1
Argha <arghagolui@gmail.com>
2023-04-12 17:06:26 +0530

    video 16: adding blog json as data

--- pages/blog.js
+++ pages/blog.js
@@ -2,13 +2,20 @@ import React from "react";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 
+
+
+// task 1: collect all files from blogdata directory
+// task 2: ierate throgh them and display them
 const blog = () => {
   return (
+    <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.blogs}>
           <h2>Popular Blogs</h2>
           <div className={styles.blogitem}>
-            <Link href='/blogpost/letslearnjavascript' ><h3>How to learn JavaScript in 2023</h3></Link>
+            <Link href="/blogpost/letslearnjavascript">
+              <h3>How to learn JavaScript in 2023</h3>
+            </Link>
             <p>
               JavaScript is the language used to design the logic for the web
             </p>
@@ -27,6 +34,7 @@ const blog = () => {
           </div>
         </div>
       </main>
+    </div>
   );
 };
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
 import styles from "@/styles/Home.module.css";
 const slug = () => {
-    const router=useRouter();
-    const {slug}= router.query;
+  //task 1: find the file corressponding to slug
+  //task 2: populate them inside the page
+  const router = useRouter();
+  const { slug } = router.query;
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-      <h1>{slug}</h1>
-      <hr />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolor accusantium consectetur rem, dolores ad atque voluptatum rerum blanditiis reiciendis!</p>
+        <h1>{slug}</h1>
+        <hr />
+        <hr />
+        <div style={{padding:'1rem'}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          dolor accusantium consectetur rem, dolores ad atque voluptatum rerum
+          blanditiis reiciendis!
+        </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default slug
+export default slug;
