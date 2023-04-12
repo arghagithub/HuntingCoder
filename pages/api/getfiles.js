f78e7564c0809bb9a9f9cbfef8ab14ec0fd16ba0
Argha <arghagolui@gmail.com>
2023-04-13 01:22:18 +0530

    video 19: Using the API Endpoints

--- pages/api/getfiles.js
+++ pages/api/getfiles.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
 const path = require("path");
-
+// It is just for creating and endpoint like a api
 const directoryPath = "blogdata";
 
-export default function handler(req, res) {
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.log(err);
-    } else {
-      files.forEach((file) => {
-        console.log(path.join(directoryPath, file));
-        res.json(files);
-      });
-    }
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir(directoryPath);
+  let myfile,
+    allblogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const file = data[index];
+    console.log(file);
+    myfile = await fs.promises.readFile(`blogdata/${file}`, "utf-8");
+    allblogs.push(JSON.parse(myfile));
+  }
+
+  res.status(200).json(allblogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,37 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
-
-
 
 // task 1: collect all files from blogdata directory
 // task 2: ierate throgh them and display them
 const blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/getfiles")
+      .then((data) => {
+        return data.json();
+      })
+      .then((parsedata) => {
+        console.log(parsedata);
+        setBlogs(parsedata);
+      });
+  }, []);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.blogs}>
           <h2>Popular Blogs</h2>
-          <div className={styles.blogitem}>
-            <Link href="/blogpost/letslearnjavascript">
-              <h3>How to learn JavaScript in 2023</h3>
-            </Link>
-            <p>
-              JavaScript is the language used to design the logic for the web
-            </p>
-          </div>
-          <div className={styles.blogitem}>
-            <h3>How to learn JavaScript in 2023</h3>
-            <p>
-              JavaScript is the language used to design the logic for the web
-            </p>
-          </div>
-          <div className={styles.blogitem}>
-            <h3>How to learn JavaScript in 2023</h3>
-            <p>
-              JavaScript is the language used to design the logic for the web
-            </p>
-          </div>
+          {blogs.map((blogitem,index) => {
+            return (
+              <div className={styles.blogitem} key={index}>
+                <Link href={`/blogpost/${blogitem.slug}`}>
+                  <h3>{blogitem.title}</h3>
+                </Link>
+                <p>
+                  {blogitem.content}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
